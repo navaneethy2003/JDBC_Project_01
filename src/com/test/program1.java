bb044abdd29fe77f4200630b5e1dc0190935bff8
navaneethy2003 <152059705+navaneethy2003@users.noreply.github.com>
2025-05-06 09:31:52 +0530

    Update program1.java

--- src/com/test/program1.java
+++ src/com/test/program1.java
@@ -3,7 +3,13 @@ package com.test;
 public class program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n=s.nextInt();
+		System.out.println("square of"+n+" is :"+(n*n));
+		
+		
 
 	}
 
